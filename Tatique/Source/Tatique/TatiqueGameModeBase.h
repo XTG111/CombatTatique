086d96a72415f3733b45bc2889e36a5573199820
XTG111 <908490775@qq.com>
2024-03-19 20:12:46 +0800

    make gamemode pawn and controller

---
+++ Tatique/Source/Tatique/TatiqueGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TatiqueGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TATIQUE_API ATatiqueGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
